cbc77a298b10f094560637cdd7388622ea126d93
chetandasauni <chetandasauni25@gmail.com>
2021-10-27 14:58:37 -0700

    fixed the css for placeholder component

--- components/placeholder.tsx
+++ components/placeholder.tsx
@@ -2,15 +2,37 @@
 import Skeleton from '@mui/material/Skeleton';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme: any) => ({
+   placeholderWrapper: {
+    flexGrow: 1,
+    margin: '5rem',
+    [theme.breakpoints.down('sm')]: {
+        margin: '5rem 2rem',
+        width: '100%'
+      },
+   },
+   imageHolder: {
+    width: '300px',
+    height: '300px',
+    [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        height: '100%'
+      },
+   }
+}));
 
 const DisplayPlaceholder = () => {
+    const styles = useStyles();
+
     return (
-        <Box sx={{ flexGrow: 1, margin: '5rem' }}>
+        <Box className={styles.placeholderWrapper}>
             <Grid container spacing={1}>
-                <Grid item xs={3}>
-                   <Skeleton variant="rectangular" width={300} height={300} />
+                <Grid item xs={5}>
+                   <Skeleton variant="rectangular" className={styles.imageHolder} />
                 </Grid>
-                <Grid item xs={9}>
+                <Grid item xs={6}>
                     <Box sx={{ pt: 0.5 }}>
                         <Skeleton width="30%" />
                         <Skeleton width="80%" />
